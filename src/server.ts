65c818219d4c6668e04eb1de5a536574a6c6391b
Novo1999 <neptune.navo@gmail.com>
2023-10-22 17:24:52 +0600

    server

--- src/server.ts
+++ src/server.ts
@@ -14,18 +14,18 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import helmet from 'helmet'
 import ExpressMongoSanitize from 'express-mongo-sanitize'
-// import cors from 'cors'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 
-// app.use(
-//   cors({
-//     origin: [],
-//     methods: ['POST', 'GET'],
-//     credentials: true,
-//   })
-// )
+app.use(
+  cors({
+    origin: [''],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
