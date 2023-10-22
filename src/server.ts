580da029ec290dd56697ba68f38ec2ab241e7b8c
Novo1999 <neptune.navo@gmail.com>
2023-10-22 17:41:48 +0600

    frontend path

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ const DATABASE_URL = process.env.MONGO_URL!
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
-app.use(express.static(path.resolve(__dirname, './client/dist')))
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
 app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
@@ -45,7 +45,7 @@ app.use('/api/v1/kanban', authenticateUser as () => void, kanbanRouter)
 app.use('/api/v1/users', authenticateUser as () => void, userRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
 })
 
 app.use('*', (req, res) => {
